8c7fac73a93766685e9167f5bb0b4b00a540370a
Raosahab98 <iamashish986@gmail.com>
2024-06-20 04:29:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,6 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '763351261').split()]
+USERNAME = environ.get('USERNAME', 'https://telegram.me/DOCTORx98')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '763351261').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
