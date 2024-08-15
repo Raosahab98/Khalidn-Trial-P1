36f7bb089e0e29a19539d7ab3f411f05f8a8a427
Raosahab98 <iamashish986@gmail.com>
2024-08-15 19:15:38 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -73,9 +73,6 @@ async def save_group(bot, message):
         if settings["auto_delete"]:
             await asyncio.sleep(600)
             await (temp.MELCOW['welcome']).delete()
-                
-               
-
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
