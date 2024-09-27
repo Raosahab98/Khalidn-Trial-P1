f3d0a166d879088e3656c46751e74d996dab1bd3
Raosahab98 <iamashish986@gmail.com>
2024-09-28 02:57:26 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -5,7 +5,6 @@ from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from googletrans import Translator
-from googletrans.constants import LANGUAGES
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
