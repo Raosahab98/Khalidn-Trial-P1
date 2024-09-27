0ba709efece9a9d65f426331dd600fb815992f0e
Raosahab98 <iamashish986@gmail.com>
2024-09-28 02:45:36 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -4,6 +4,8 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, Media
 from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
+from googletrans import Translator
+from googletrans.constants import LANGUAGES
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
