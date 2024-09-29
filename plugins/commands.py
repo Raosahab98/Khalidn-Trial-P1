dbdfe5dfc692c11cf3ff963d681ee112c3616d3d
Raosahab98 <iamashish986@gmail.com>
2024-09-30 01:11:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -357,8 +357,7 @@ async def start(client, message):
         if AUTO_DELETE_MODE == True:
             k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
             await asyncio.sleep(AUTO_DELETE_TIME)
-            for x in filesarr:
-            await k.delete()
+            await msg.delete()
             await msg.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏꜱ / ꜰɪʟᴇꜱ ᴀʀᴇ ᴅᴇʟᴇᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ !!!</b>")
          return
         except:
