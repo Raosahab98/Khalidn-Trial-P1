de6a75336bdf79753595db0973f6b8943baba757
Raosahab98 <iamashish986@gmail.com>
2024-09-30 00:34:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -338,9 +338,9 @@ async def start(client, message):
                 return
             msg = await client.send_cached_media(
                 chat_id=message.from_user.id,
-                file_id=msg.get("file_id"),
-                    caption=f_caption,
-                    protect_content=msg.get('protect', False),
+                file_id=file_id,
+                caption=f_caption,
+                protect_content=True if pre == 'filep' else False,
                 reply_markup=(
                     InlineKeyboardMarkup(
                         [
