bf6d7d75cc5d49788f42a578f865954d1fe7d562
Raosahab98 <iamashish986@gmail.com>
2024-10-06 14:39:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,6 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002197302739'))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://a2blinks99:Samael@samaelxbot.77wiky2.mongodb.net/?retryWrites=true&w=majority")
